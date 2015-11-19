6e11db60c2ce45d0bb9c698548226a48e7e105a1
Mariko <maripiyok@gmail.com>
2015-11-19 12:22:37 +0900

    Add name to user model.
    Run migration.

--- db/migrate/20151119003625_devise_create_users.rb
+++ db/migrate/20151119003625_devise_create_users.rb
@@ -1,6 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.string :name, null: false, default: ""
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -34,6 +35,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps null: false
     end
 
+    add_index :users, :name, unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
